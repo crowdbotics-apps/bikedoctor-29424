5b6201fbfe2a989b077bd723fceaf3722c3b2f97
Crowdbotics <team@crowdbotics.com>
2021-08-03 20:03:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "bikedoctor_29424Identifier",
+  url: "https://bikedoctor_29424Identifier.botics.co",
+  api: "https://bikedoctor_29424Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
