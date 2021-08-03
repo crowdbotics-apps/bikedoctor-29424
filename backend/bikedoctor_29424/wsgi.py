c69b97f6482a9cd36d1602cb6238b6a4346ef9f4
Crowdbotics <team@crowdbotics.com>
2021-08-03 20:04:56 +0000

    Update models

--- backend/bikedoctor_29424/wsgi.py
+++ backend/bikedoctor_29424/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bikedoctor_29424.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bikedoctor_29424.settings")
 
 application = get_wsgi_application()
